a29c182ab06d85882b90af65b32e15ed2d318fdf
Michael Fluharty <michael@fluharty.com>
2026-09-04 07:54:23 -0500

    Initial Commit

---
+++ Shell Citadel/Shell_CitadelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shell_CitadelApp.swift
+//  Shell Citadel
+//
+//  Created by Michael Fluharty on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Shell_CitadelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shell CitadelTests/Shell_CitadelTests.swift
@@ -0,0 +1,18 @@
+//
+//  Shell_CitadelTests.swift
+//  Shell CitadelTests
+//
+//  Created by Michael Fluharty on 9/4/26.
+//
+
+import Testing
+
+struct Shell_CitadelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
